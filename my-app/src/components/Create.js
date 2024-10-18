a2f4cd0f01bfd35c9d554bfc7beee5fc4b137794
CianDickerHughes <g00415413@atu.ie>
2024-10-18 14:23:42 +0000

    added movie year and movie poster to the form

--- my-app/src/components/Create.js
+++ my-app/src/components/Create.js
@@ -1,19 +1,22 @@
 import { useState } from "react";
 
-
-
 // Return the Create
 const Create = () => {
 
+  // array of movies of json
   // explain: it's a react hook that allows you to add state to a functional component
   const [title, setTitle] = useState('');
+  const [year, setYear] = useState('');
+  const [poster, setPoster] = useState('');
 
-
+  // hands the sumbmits of title of the movies
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(title);
+    const movie = {title, year, poster};
+    console.log(movie);
   }
 
+  // form to upload movies to the server
   return (
     <div>
       <h2>This is my Create Component.</h2>
@@ -23,8 +26,20 @@ const Create = () => {
           <input type="text"
             className="form-control"
             value={title}
-            onChange={(e) => { setTitle(e.target.value) }}
-          />
+            onChange={(e) => { setTitle(e.target.value) }} /* user add movie title*/ />
+          <br/>
+          <label>Add Movie Year: </label>
+          <input type="text"
+            className="form-control"
+            value={year}
+            onChange={(e) => { setYear(e.target.value) }} /* user add movie year*/ />
+          <br/>
+          <label>Add Movie Poster: </label>
+          <input type="text"
+            className="form-control"
+            value={poster}
+            onChange={(e) => { setPoster(e.target.value) }} /* user add movie poster*/ />
+          <br/>
         </div>
         <input type="submit" value="Add Movie" />
       </form>
