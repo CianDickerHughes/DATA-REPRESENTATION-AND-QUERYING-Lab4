26651ac8b980a7e147493225b6c381516835061a
CianDickerHughes <g00415413@atu.ie>
2024-10-18 13:41:07 +0000

    can call movies from a json by http

--- my-app/src/components/Read.js
+++ my-app/src/components/Read.js
@@ -1,37 +1,28 @@
 import Movies from "./Movies";
+import { useEffect, useState  } from 'react';
+import axios from "axios";
 
 // The Read
 const Read = () => {
-    // JSON of movies
-    const data = [
-        // {
-        // "Title": "Avengers: Infinity War",
-        // "Year": "2018",
-        // "imdbID": "tt4154756",
-        // "Type": "movie",
-        // "Poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
-        // },
-        // {
-        // "Title": "Captain America: Civil War",
-        // "Year": "2016",
-        // "imdbID": "tt3498820",
-        // "Type": "movie",
-        // "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
-        // },
-        // {
-        // "Title": "World War Z",
-        // "Year": "2013",
-        // "imdbID": "tt0816711",
-        // "Type": "movie",
-        // "Poster": "https://m.media-amazon.com/images/M/MV5BNDQ4YzFmNzktMmM5ZC00MDZjLTk1OTktNDE2ODE4YjM2MjJjXkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg"
-        // }
-    ];
+    // array of movies of json
+    const [movies, setMovies] = useState([]);
+
+    // get movies from http
+    useEffect(() => {
+        axios.get('https://jsonblob.com/api/jsonblob/1287718524221775872') // json http
+          .then((response) => {
+            setMovies(response.data.movies);
+          })
+          .catch((error) => { // error function
+            console.log(error);
+          });
+      }, []);
     
     // return read component
     return (
         <div>
             <h3>Hello from read component!</h3>
-            <Movies myMovies={data}/*PASS data to Movies.js*//>
+            <Movies myMovies={movies}/*PASS data to Movies.js*//>
         </div>
     );
   }
